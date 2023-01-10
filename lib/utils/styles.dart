75ed48f45134acab801292187e74d291c58f83ba
Chaerulf <chairulfauzi27@gmail.com>
2023-01-10 10:36:39 +0700

    add home page and add some folder

---
+++ lib/utils/styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kColorPrimary = Color(0x00007bff);
+const Color kColorSecondary = Colors.white;
+const Color kColorIndigo = Color(0x006610f2);
+const Color kColorNavy = Color.fromARGB(255, 6, 24, 51);
+const Color kcategoryColor = Colors.black26;
+
+final textTheme = TextTheme(
+  headline1: GoogleFonts.inter(
+      fontSize: 93, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.inter(
+      fontSize: 58, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.inter(fontSize: 47, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.inter(
+      fontSize: 33, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.inter(
+      fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.inter(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.inter(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.inter(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.inter(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.inter(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.inter(
+      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.inter(
+      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
